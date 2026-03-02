c249deadbfeb89b403aa3ca8c2bba174f02c4851
Pushkar Kadam <pushkarkadam17@outlook.com>
2026-03-02 16:56:32 +1100

    example code updated for ground truth checkpoint

--- examples/handtrack/checkpoint_ground_truth.py
+++ examples/handtrack/checkpoint_ground_truth.py
@@ -6,3 +6,11 @@ import seamdet as sd
 
 
 data = sd.path.point_plotter('rectified_imageL.png', 'checkpoints', ["check_points"], ["x"], ["r"])
+
+loaded_data = sd.path.load_checkpoint_gt('checkpoints.pkl')
+
+try: 
+    assert(data == loaded_data)
+    print('\033[92m' + "Success!")
+except:
+    print('\033[91m' + "Loaded data does not match the return data.")
