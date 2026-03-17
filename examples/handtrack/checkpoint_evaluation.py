32e327800cf4899c073167058e096cfe92c7d710
Pushkar Kadam <pushkarkadam17@outlook.com>
2026-03-17 17:37:05 +1100

    saving evaluation data in pickle and yaml file

--- examples/handtrack/checkpoint_evaluation.py
+++ examples/handtrack/checkpoint_evaluation.py
@@ -9,6 +9,7 @@ import copy
 import pandas as pd
 import pickle
 import matplotlib.pyplot as plt
+import yaml
 
 sys.path.append('../..')
 
@@ -53,6 +54,8 @@ if __name__ == '__main__':
 
     visited_checkpoints, passing_ratio = sd.path.evaluate_checkpoints(chk_pts, seam_path_dict, box_extension)
 
+    print(f'Passing ratio: {passing_ratio}')
+
     not_visited_chk = list(set(chk_pts) - set(visited_checkpoints))
 
     xv, yv = zip(*visited_checkpoints)
@@ -61,14 +64,30 @@ if __name__ == '__main__':
     else:
         xnv, ynv = [], []
 
+    # Saving evaluation results
+    evaluation_metric = {'visited_checkpoints': visited_checkpoints,
+                         'not_visited_checkpoints': not_visited_chk,
+                         'passing_ratio': passing_ratio}
+
+    evaluation_metric_path = os.path.join(track_path, 'evaluation_metric_data.pkl')
+
+    with open(evaluation_metric_path, 'wb') as f:
+        pickle.dump(evaluation_metric, f)
+
+    evaluation_metric_result = {'passing_ratio': passing_ratio}
+
+    evaluation_yaml_path = os.path.join(track_path, 'evaluation_data.yml')
+
+    with open(evaluation_yaml_path, 'w') as f:
+        yaml.dump(evaluation_metric_result, f)
 
     plt.imshow(rectL)
     plt.plot(xl, yl, color='purple', alpha=0.5, label='Seam Path')
     plt.scatter(xv, yv, color='g', marker='D', label='Visited checkpoints')
     plt.scatter(xnv, ynv, color='r', marker='X', label='Not visited checkpoints')
     plt.legend()
-    plt.grid()
-    plt.savefig(os.path.join(track_path, 'checkpoint_path.png'))
+    plt.axis("off")
+    plt.savefig(os.path.join(track_path, 'checkpoint_path.png'), bbox_inches='tight', pad_inches=0)
 
 
 
